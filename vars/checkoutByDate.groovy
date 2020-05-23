20f0273b6d1000cde3e54f9dec68959a407f4e5f
Marius Keppler <mkeppler@raxio.eu>
2020-05-23 10:41:31 +0200

    checkoutDate fixed

--- vars/checkoutByDate.groovy
+++ vars/checkoutByDate.groovy
@@ -2,8 +2,8 @@ def call(String checkoutDate){
     println("checkoutByDate using $checkoutDate")
 
     if(!checkoutDate.equals("now")){
-        sh 'git rev-list -1 --before="$checkoutdate" --date="format:dd.mm.yyyy" origin/master'
-        sh 'git checkout `git rev-list -1 --before="$checkoutdate" --date="format:dd.mm.yyyy" origin/master`'
+        sh "git rev-list -1 --before=\"$checkoutDate\" --date=\"format:dd.mm.yyyy\" origin/master"
+        sh "git checkout `git rev-list -1 --before=\"$checkoutDate\" --date=\"format:dd.mm.yyyy\" origin/master`"
     }else{
         println("do nothing because checkoutdate is now")
     }
