2721eaadc3e498cea45e1e505cab0f6924e5a515
Marius Keppler <mkeppler@raxio.eu>
2020-05-23 08:39:41 +0200

    Having durationEvent as shared lib

--- vars/durationEvent.groovy
+++ vars/durationEvent.groovy
@@ -1,3 +1,5 @@
-def call() {
-    println "hello from durationEvent"
+def call(def event, def code) {
+    println "hello from durationEvent: $event"
+    code()
+    println "hello from durationEvent: $event"
 }
