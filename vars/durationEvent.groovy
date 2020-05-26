3210f7205a4f70812d4c7bd3ce8bf1b018235005
Fabian Fichter <faficode@gmail.com>
2020-05-26 20:04:35 +0200

    end duration in finally block

--- vars/durationEvent.groovy
+++ vars/durationEvent.groovy
@@ -2,9 +2,12 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 def call(def event, Closure closure) {
-    def startTs = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-    echo "${startTs}|duration|start|$event"
-    closure()
-    def endTs = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
-    echo "${endTs}|duration|end|$event"
+    try {
+        def startTs = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        echo "${startTs}|duration|start|$event"
+        closure()
+    } finally {
+        def endTs = ZonedDateTime.now().format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        echo "${endTs}|duration|end|$event"
+    }
 }
